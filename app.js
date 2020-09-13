e41ca4d6a0b1ba89bd3003ebc7ebd0f70d37029f
Yalin Eren Deliorman <yalin.deliorman@gmail.com>
2020-09-13 14:47:42 +0300

    code
    simple counter for js

---
+++ app.js
@@ -0,0 +1,29 @@
+let count = 0;
+
+const value = document.querySelector('#value');
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(function (button) {
+    button.addEventListener('click', function (e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains('decrease')) {
+            count--;
+        } else if (styles.contains('increase')) {
+            count++;
+        } else {
+            count = 0;
+        }
+
+        if (count > 0) {
+            value.style.color = "green";
+        }
+        if (count < 0) {
+            value.style.color = "red";
+        }
+        if (count == 0) {
+            value.style.color = "black";
+        }
+
+        value.textContent = count;
+    })
+});
